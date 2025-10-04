2bd027cf7f7fbe5f85474a3b88de61dea99be2d8
Amy Gonzalez <amaice8@gmail.com>
2025-10-04 12:54:16 -0500

    Working on message bubble and screen design

--- app/src/main/java/com/example/bonfire/MainActivity.kt
+++ app/src/main/java/com/example/bonfire/MainActivity.kt
@@ -14,18 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
+
+        // Prevent dark mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         val rootView = findViewById<View?>(android.R.id.content)
 
-        ViewCompat.setOnApplyWindowInsetsListener(
-            rootView,
-            OnApplyWindowInsetsListener { v: View?, insets: WindowInsetsCompat? ->
+        if (rootView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(
+                rootView
+            ) { v: View?, insets: WindowInsetsCompat? ->
                 val imeInsets: Insets = insets!!.getInsets(WindowInsetsCompat.Type.ime())
                 val navInsets: Insets = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
-
-
-                val bottomInset: Int = Math.max(imeInsets.bottom, navInsets.bottom)
+                val bottomInset: Int = imeInsets.bottom.coerceAtLeast(navInsets.bottom)
 
                 rootView.setPadding(
                     navInsets.left,
@@ -34,6 +35,7 @@ class MainActivity : AppCompatActivity() {
                     bottomInset
                 )
                 insets
-            })
+            }
+        }
     }
 }
