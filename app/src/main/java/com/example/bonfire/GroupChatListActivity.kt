ecd70c3111550d2cd14044ef158c97e6a7cd1b7b
Amy Gonzalez <amaice8@gmail.com>
2025-10-15 22:56:32 -0500

    connects to firebase, sign in/out

---
+++ app/src/main/java/com/example/bonfire/GroupChatListActivity.kt
@@ -0,0 +1,38 @@
+package com.example.bonfire
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.auth.FirebaseAuth
+
+class GroupChatListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.groupchat_list_layout)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        // for testing
+        val user = FirebaseAuth.getInstance().currentUser
+        Toast.makeText(baseContext, "Signed in! Welcome " + user?.email, Toast.LENGTH_SHORT).show()
+
+        val openChatButton: ImageButton = findViewById(R.id.text_chat_list_message)
+        openChatButton.setOnClickListener {
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+
+        // log out
+        val logoutButton: ImageButton = findViewById(R.id.menu_button_logout)
+        logoutButton.setOnClickListener {
+            // go back to welcome screen
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
