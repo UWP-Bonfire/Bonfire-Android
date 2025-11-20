667c1f1c1c1894a55ca230d0f2a2f29c3e7df454
Amy Gonzalez <amaice8@gmail.com>
2025-11-19 23:52:49 -0600

    changing pfp to new url path

--- app/src/main/java/com/example/bonfire/AccountActivity.kt
+++ app/src/main/java/com/example/bonfire/AccountActivity.kt
@@ -84,7 +84,7 @@ class AccountActivity : AppCompatActivity() {
             child.setOnClickListener {
                 // Update the "avatar" field of the user 
                 val userRef = db.collection("users").document(user?.uid ?: "")
-                val avatar = "/images/icon" + (i + 1).toString()  + ".png"
+                val avatar = "/bonfire-backend/src/assets/icons/${(i + 1)}.png"
                 userRef
                     .update("avatar", avatar)
                     .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully updated!") }
